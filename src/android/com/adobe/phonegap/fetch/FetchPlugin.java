0be0025021ac04890965858a6e8a8b54fca4e264
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-03-21 16:51:35 +0800

    Update FetchPlugin.java

--- src/android/com/adobe/phonegap/fetch/FetchPlugin.java
+++ src/android/com/adobe/phonegap/fetch/FetchPlugin.java
@@ -11,6 +11,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.Call;
+import okhttp3.ConnectionPool;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -21,6 +22,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import static online.streamely.www.OkHttpClientExtensionsKt.ignoreAllSSLErrors;
+import static OkHttpClientExtensionsKt.ignoreAllSSLErrors;
 
 public class FetchPlugin extends CordovaPlugin {
 
@@ -31,142 +34,144 @@ public class FetchPlugin extends CordovaPlugin {
     public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
 
     private static final long DEFAULT_TIMEOUT = 180;
-
-    @Override
+@Override
     public boolean execute(final String action, final JSONArray data, final CallbackContext callbackContext) {
-
         if (action.equals("fetch")) {
-
-            try {
-                String method = data.getString(0);
-                Log.v(LOG_TAG, "execute: http method  = " + method.toString());
-
-                String urlString = data.getString(1);
-                Log.v(LOG_TAG, "execute: urlString = " + urlString.toString());
-
-                String postBody = data.getString(2);
-                Log.v(LOG_TAG, "execute: postBody = " + postBody.toString());
-
-                JSONObject headers = data.getJSONObject(3);
-                if (headers.has("map") && headers.getJSONObject("map") != null) {
-                    headers = headers.getJSONObject("map");
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    fetchOperation(data, callbackContext);
                 }
-
-                Log.v(LOG_TAG, "execute: headers = " + headers.toString());
-
-                Request.Builder requestBuilder = new Request.Builder();
-
-                // method + postBody
-                if (postBody != null && !postBody.equals("null")) {
-                    // requestBuilder.post(RequestBody.create(MEDIA_TYPE_MARKDOWN, postBody.toString()));
-                    String contentType;
-                     if (headers.has("content-type")) {
-                         JSONArray contentTypeHeaders = headers.getJSONArray("content-type");
-                         contentType = contentTypeHeaders.getString(0);
-                     } else {
-                         contentType = "application/json";
-                     }
-                     requestBuilder.post(RequestBody.create(MediaType.parse(contentType), postBody.toString()));
-                } else {
-                    requestBuilder.method(method, null);
-                }
-
-                // url
-                requestBuilder.url(urlString);
-
-                // headers
-                if (headers != null && headers.names() != null && headers.names().length() > 0) {
-                    for (int i = 0; i < headers.names().length(); i++) {
-
-                        String headerName = headers.names().getString(i);
-                        JSONArray headerValues = headers.getJSONArray(headers.names().getString(i));
-
-                        if (headerValues.length() > 0) {
-                            String headerValue = headerValues.getString(0);
-                            Log.v(LOG_TAG, "key = " + headerName + " value = " + headerValue);
-                            requestBuilder.addHeader(headerName, headerValue);
-                        }
-                    }
-                }
-
-                Request request = requestBuilder.build();
-
-                mClient.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException throwable) {
-                        throwable.printStackTrace();
-                        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, throwable.getMessage()));
-                    }
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-
-                        JSONObject result = new JSONObject();
-                        try {
-                            Headers responseHeaders = response.headers();
-
-                            JSONObject allHeaders = new JSONObject();
-
-                            if (responseHeaders != null ) {
-                                for (int i = 0; i < responseHeaders.size(); i++) {
-                                    if (responseHeaders.name(i).compareToIgnoreCase("set-cookie") == 0 &&
-                                        allHeaders.has(responseHeaders.name(i))) {
-                                        allHeaders.put(responseHeaders.name(i), allHeaders.get(responseHeaders.name(i)) + ",\n" + responseHeaders.value(i));
-                                        continue;
-                                    }
-                                    allHeaders.put(responseHeaders.name(i), responseHeaders.value(i));
-                                }
-                            }
-
-                            result.put("headers", allHeaders);
-
-                            if (response.body().contentType() != null && response.body().contentType().type().equals("image")) {
-                                result.put("isBlob", true);
-                                result.put("body", Base64.encodeToString(response.body().bytes(), Base64.DEFAULT));
-                            } else {
-                                result.put("body", response.body().string());
-                            }
-
-                            result.put("statusText", response.message());
-                            result.put("status", response.code());
-                            result.put("url", response.request().url().toString());
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        Log.v(LOG_TAG, "HTTP code: " + response.code());
-                        Log.v(LOG_TAG, "returning: " + result.toString());
-
-                        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
-                    }
-                });
-
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "execute: Got JSON Exception " + e.getMessage());
-                callbackContext.error(e.getMessage());
-            }
-
-        } 
-        else if (action.equals("setTimeout")) {
+            });
+            return true;
+        } else if (action.equals("setTimeout")) {
             this.setTimeout(data.optLong(0, DEFAULT_TIMEOUT));
-        }
-        else {
-            Log.e(LOG_TAG, "Invalid action : " + action);
+            return true;
+        } else {
+            Log.e(LOG_TAG, "Invalid action: " + action);
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
             return false;
         }
+    }
 
-        return true;
+    private void fetchOperation(JSONArray data, CallbackContext callbackContext) {
+        try {
+            String method = data.getString(0);
+            Log.v(LOG_TAG, "execute: http method  = " + method.toString());
+
+            String urlString = data.getString(1);
+            Log.v(LOG_TAG, "execute: urlString = " + urlString.toString());
+
+            String postBody = data.getString(2);
+            Log.v(LOG_TAG, "execute: postBody = " + postBody.toString());
+
+            JSONObject headers = data.getJSONObject(3);
+            if (headers.has("map") && headers.getJSONObject("map") != null) {
+                headers = headers.getJSONObject("map");
+            }
+
+            Log.v(LOG_TAG, "execute: headers = " + headers.toString());
+
+            Request.Builder requestBuilder = new Request.Builder();
+
+            // method + postBody
+            if (postBody != null && !postBody.equals("null")) {
+                String contentType;
+                if (headers.has("content-type")) {
+                    JSONArray contentTypeHeaders = headers.getJSONArray("content-type");
+                    contentType = contentTypeHeaders.getString(0);
+                } else {
+                    contentType = "application/json";
+                }
+                requestBuilder.post(RequestBody.create(MediaType.parse(contentType), postBody.toString()));
+            } else {
+                requestBuilder.method(method, null);
+            }
+
+            // url
+            requestBuilder.url(urlString);
+
+            // headers
+            if (headers != null && headers.names() != null && headers.names().length() > 0) {
+                for (int i = 0; i < headers.names().length(); i++) {
+                    String headerName = headers.names().getString(i);
+                    JSONArray headerValues = headers.getJSONArray(headers.names().getString(i));
+
+                    if (headerValues.length() > 0) {
+                        String headerValue = headerValues.getString(0);
+                        Log.v(LOG_TAG, "key = " + headerName + " value = " + headerValue);
+                        requestBuilder.addHeader(headerName, headerValue);
+                    }
+                }
+            }
+
+            Request request = requestBuilder.build();
+
+            mClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException throwable) {
+                    throwable.printStackTrace();
+                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, throwable.getMessage()));
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    handleResponse(response, callbackContext);
+                }
+            });
+
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "execute: Got JSON Exception " + e.getMessage());
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void handleResponse(Response response, CallbackContext callbackContext) throws IOException {
+        JSONObject result = new JSONObject();
+        try {
+            Headers responseHeaders = response.headers();
+            JSONObject allHeaders = new JSONObject();
+
+            if (responseHeaders != null) {
+                for (int i = 0; i < responseHeaders.size(); i++) {
+                    if (responseHeaders.name(i).compareToIgnoreCase("set-cookie") == 0 &&
+                            allHeaders.has(responseHeaders.name(i))) {
+                        allHeaders.put(responseHeaders.name(i), allHeaders.get(responseHeaders.name(i)) + ",\n" + responseHeaders.value(i));
+                        continue;
+                    }
+                    allHeaders.put(responseHeaders.name(i), responseHeaders.value(i));
+                }
+            }
+
+            result.put("headers", allHeaders);
+
+            if (response.body().contentType() != null && response.body().contentType().type().equals("image")) {
+                result.put("isBlob", true);
+                result.put("body", Base64.encodeToString(response.body().bytes(), Base64.DEFAULT));
+            } else {
+                result.put("body", response.body().string());
+            }
+
+            result.put("statusText", response.message());
+            result.put("status", response.code());
+            result.put("url", response.request().url().toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.v(LOG_TAG, "HTTP code: " + response.code());
+        Log.v(LOG_TAG, "returning: " + result.toString());
+
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
     }
 
     private void setTimeout(long seconds) {
         Log.v(LOG_TAG, "setTimeout: " + seconds);
-
-	mClient = mClient.newBuilder()
-          .connectTimeout(seconds, TimeUnit.SECONDS)
-          .readTimeout(seconds, TimeUnit.SECONDS)
-          .writeTimeout(seconds, TimeUnit.SECONDS)
-	  .build();
+        mClient = ignoreAllSSLErrors(mClient.newBuilder())
+            .connectionPool(new ConnectionPool(5, seconds, TimeUnit.SECONDS))
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build();
     }
 }
