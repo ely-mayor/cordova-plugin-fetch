13ed263bf5b7e52d428fa26f14dcc757744b734a
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-03-22 12:03:15 +0800

    Update FetchPlugin.java

--- src/android/com/adobe/phonegap/fetch/FetchPlugin.java
+++ src/android/com/adobe/phonegap/fetch/FetchPlugin.java
@@ -19,6 +19,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLSocketFactory;
 
 import okhttp3.Callback;
 import okhttp3.Headers;
