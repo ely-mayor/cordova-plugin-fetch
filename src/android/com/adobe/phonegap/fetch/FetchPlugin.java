8d83a2f4dbf644f492d7ae0e53f9babc34327ee8
elyeandre <96169920+ely-mayor@users.noreply.github.com>
2024-03-27 20:31:58 +0800

    Update FetchPlugin.java

--- src/android/com/adobe/phonegap/fetch/FetchPlugin.java
+++ src/android/com/adobe/phonegap/fetch/FetchPlugin.java
@@ -35,7 +35,7 @@ public class FetchPlugin extends CordovaPlugin {
     private OkHttpClient mClient = new OkHttpClient();
     public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
 
-    private static final long DEFAULT_TIMEOUT = 180;
+    private static final long DEFAULT_TIMEOUT = 10;
 	
 @Override
     public boolean execute(final String action, final JSONArray data, final CallbackContext callbackContext) {
